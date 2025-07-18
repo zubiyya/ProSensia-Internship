2eeea88253efd1ce1c000a7e2d7b6208a3b60dce
zubiyya <zubiatanolii17@gmail.com>
2025-07-18 16:36:30 +0500

    Added day 16 task

---
+++ Day16/todo_utils.py
@@ -0,0 +1,22 @@
+
+import json
+import os
+from datetime import datetime
+
+DATA_FILE = "todo_data.json"
+LOG_FILE = "logs.txt"
+
+def load_tasks():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(DATA_FILE, "w") as file:
+        json.dump(tasks, file, indent=2)
+
+def log_action(action):
+    with open(LOG_FILE, "a") as log:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log.write(f"[{timestamp}] {action}\n")
